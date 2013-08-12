ad95e615ae3be4530320904780868cedcb7f74d5
vikasdhi <vikasdhi@898a188c-9671-0410-8e00-e3fd810bbb7f>
2013-08-12 15:02:46 +0000

    reverting back to slower version of inference_gibbs_sampling.
    git-svn-id: https://svn.cc.gatech.edu/borg/OccupancyGrid/trunk@18224 898a188c-9671-0410-8e00-e3fd810bbb7f

--- OccupancyGrid/include/forward_sensor_model.h
+++ OccupancyGrid/include/forward_sensor_model.h
@@ -10,6 +10,18 @@
 // from stage/libstage/model_laser.cc
 #define LASER_MAX_RANGE 8.0 // in meters
 
+class Observation2D {
+  public:
+    double px, py, ptheta, range;
+
+    Observation2D() {}
+
+    Observation2D(double px, 
+        double py,
+        double ptheta,
+        double range) : px(px),py(py), ptheta(ptheta), range(range) {}
+};
+
 // Abondoned because requires refactoring of ray_trace method to accept a
 // function pointer instead of overriding a class.
 //
@@ -38,10 +50,4 @@ double log_odds_observation_given_map_and_pose(
 double log_odds_observation_given_map_and_all_poses(
     const std::vector<Observation2D>& observations,
     OccupancyGrid2D<double, int>& map);
-
-double log_odds_occupied_vs_free(
-    const std::vector<Observation2D>& observations,
-    OccupancyGrid2D<double, int>& map,
-    int k);
-
 #endif // FORWARD_SENSOR_MODEL_H
