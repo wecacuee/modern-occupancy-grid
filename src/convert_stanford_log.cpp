0ff00513ea8a6e265888ec7f56c07882030c0c46
Vikas Dhiman <vikasdhi@buffalo.edu>
2013-09-07 00:12:04 -0400

    Trying to visualize only the portion of the map that is spanned by the laser measurements.

---
+++ src/convert_stanford_log.cpp
@@ -0,0 +1,75 @@
+#include <vector>
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <fstream>
+#include "OccupancyGrid/cvmat_serialization.h"
+using namespace std;
+int main(int argc, const char *argv[])
+{
+  ifstream logfile;
+  if (argc > 1)
+    logfile.open(argv[1]);
+  std::string line;
+
+  cv::Mat laser_pose_all(0, 3, CV_64F);
+  cv::Mat laser_range_all;
+  cv::Mat scan_angles_all;
+  cv::Mat laser_reflectance_all;
+  bool first_iteration = true;
+  while (std::getline(logfile, line)) {
+    if (line.length() > 0 && line[0] == '#')
+      continue;
+    std::istringstream iss(line);
+    double timestamp; 
+    std::string host; // rossum
+    unsigned int port; // 6665
+    string interface; // laser, position, sonar
+    cv::Mat current_pose(1, 3, CV_64F);
+    if (iss >> timestamp >> host >> port >> interface) {
+      string index; // laser:0 0 part of the device address
+      double timestamp2;
+      iss >> index >> timestamp2;
+
+      if (interface == "laser") {
+        double min_angle, max_angle, step_size, count;
+        iss >> min_angle >> max_angle >> step_size >> count;
+        if (first_iteration == true) {
+          first_iteration = false;
+            laser_range_all.create(0, count, CV_64F);
+            scan_angles_all.create(0, count, CV_64F);
+            laser_reflectance_all.create(0, count, CV_8U);
+        }
+        cv::Mat range(1, count, CV_64F);
+        cv::Mat angles(1, count, CV_64F);
+        cv::Mat reflectance(1, count, CV_8U);
+        for(int i = 0; i < count; ++i) {
+          double r, intensity;
+          iss >> r >> intensity;
+          double angle = min_angle + i * (max_angle - min_angle) / (count - 1);
+          range.at<double>(0, i) = r;
+          angles.at<double>(0, i) = angle;
+          reflectance.at<double>(0, i) = 1;
+        }
+        laser_pose_all.push_back(current_pose);
+        laser_range_all.push_back(range);
+        scan_angles_all.push_back(angles);
+        laser_reflectance_all.push_back(reflectance);
+      } else if (interface == "position") {
+        double x, y, theta;
+        iss >> x >> y >> theta;
+        current_pose.at<double>(0, 0) = x;
+        current_pose.at<double>(0, 1) = y;
+        current_pose.at<double>(0, 2) = theta;
+        double vx, vy, vtheta;
+        iss >> vx >> vy >> vtheta;
+      }
+    } else {
+      std::cerr << "Error parsing line:" << line << std::endl;
+    }
+  }
+  saveMat(laser_pose_all, "laser_pose_all.bin");
+  saveMat(laser_range_all, "laser_range_all.bin");
+  saveMat(scan_angles_all, "scan_angles_all.bin");
+  saveMat(laser_reflectance_all, "laser_reflectance_all.bin");
+  return 0;
+}
--- src/visualize_ground_truth.cpp
+++ src/visualize_ground_truth.cpp
@@ -64,11 +64,11 @@ int main(int argc, char** argv) {
 
   // parse arguments ///////////////////////////////////////////
   // Declare the supported options.
-  po::options_description desc("Run dual decomposition");
+  po::options_description desc("Visualize data");
   desc.add_options()
     ("help", "produce help message")
-    ("width", po::value<double>(), "width")
-    ("height", po::value<double>(), "height")
+    ("width", po::value<double>()->required(), "Width ")
+    ("height", po::value<double>()->required(), "Height ")
     ("dir", po::value<std::string>()->default_value("."), "Data directory")
 ;
 
@@ -91,21 +91,23 @@ int main(int argc, char** argv) {
   loadMat(scan_angles, datadirectory + "/scan_angles_all.bin");
   loadMat(laser_reflectance, datadirectory + "/laser_reflectance_all.bin");
 
-    cv::Mat floorplan = cv::imread(datadirectory + "/floorplan.png",
-        cv::IMREAD_GRAYSCALE);
+  std::string floorplanfile = datadirectory + "/floorplan.png";
+  cv::Mat floorplan = cv::imread(floorplanfile, cv::IMREAD_GRAYSCALE);
 
-    //shiftPoses(laser_pose, scan_angles, laser_ranges, width, height);
-    cv::Vec2d size_bitmap(width, height);
+  cv::Vec2d original_size(width, height);
+  cv::Vec2i gridsize(floorplan.cols, floorplan.rows);
+  cv::Vec2d cellsize; 
+  cv::divide(original_size, gridsize, cellsize); // cellsize = in meters per pixel
 
-    cv::Vec2d margin(1, 1);
-    cv::Vec2d min_pt(-margin(0) - size_bitmap(0)/2, -margin(1) - size_bitmap(1)/2);
-    cv::Vec2i gridsize(floorplan.size[0], floorplan.size[1]);
-    cv::Vec2d cellsize; 
-    cv::divide(size_bitmap , gridsize, cellsize);
-    //std::cout << cellsize(0) << cellsize(1) << std::endl;
+  double origin_x = 0, origin_y = 0;
+  double size_x = width, size_y = height;
+    shiftPoses(laser_pose, scan_angles, laser_ranges, size_x, size_y, origin_x, origin_y);
+    cv::Vec2d size_bitmap(size_x, size_y);
+    cv::Vec2i size_bitmap_px(size_x / cellsize(0), size_y / cellsize(1));
+    cv::Vec2i origin_px(origin_x / cellsize(0), origin_y / cellsize(1));
+    cv::Vec2d min_pt(- size_bitmap(0)/2, - size_bitmap(1)/2);
     cv::Vec2i ncells;
-    cv::divide(min_pt, cellsize, ncells, -2);
-    //std::cout << ncells(0) << ncells(1) << std::endl;
+    cv::divide(min_pt, cellsize, ncells, -2); // ncells = number of cells in map including margins
 
     OccupancyGrid2DGT<double, int> map(
         min_pt(0), 
@@ -114,16 +116,33 @@ int main(int argc, char** argv) {
         cellsize(1),
         ncells(0),
         ncells(1));
+    map.og_ = cv::Scalar(map.OCCUPIED);
+
+    cv::Vec2i min_floorplan, max_floorplan;
+    cv::addWeighted(origin_px, 1, size_bitmap_px, -0.5, 0, min_floorplan);
+    cv::addWeighted(origin_px, 1, size_bitmap_px, 0.5, 0, max_floorplan);
+    //cv::transpose(floorplan, floorplan);
+    cv::Mat fp_roi = floorplan(
+        cv::Range(std::max(0, floorplan.rows / 2 - max_floorplan(1)),
+                  std::min(floorplan.rows / 2 - min_floorplan(1), floorplan.rows)),
+        cv::Range(std::max(0, floorplan.cols / 2 + min_floorplan(0)),
+                  std::min(floorplan.cols / 2 + max_floorplan(0), floorplan.cols))
+        );
+
+    cv::Vec2i fp_roi_size(fp_roi.cols, fp_roi.rows);
     cv::Vec2i min_bitmap;
-    cv::addWeighted(ncells, 0.5, gridsize, -0.5, 0, min_bitmap);
+    cv::addWeighted(ncells, 0.5, fp_roi_size, -0.5, 0, min_bitmap);
     cv::Vec2i max_bitmap;
-    cv::addWeighted(ncells , 0.5, gridsize, 0.5, 0, max_bitmap);
+    cv::add(min_bitmap, fp_roi_size, max_bitmap);
     printf("Map size (%d, %d)\n", ncells(0), ncells(1));
     printf("(%d, %d) <-> (%d, %d)\n", min_bitmap(0), min_bitmap(1), max_bitmap(0), max_bitmap(1));
-    cv::Mat roi = map.og_(cv::Range(min_bitmap(0), max_bitmap(0)),
+    cv::Mat map_roi = map.og_(cv::Range(min_bitmap(0), max_bitmap(0)),
       cv::Range(min_bitmap(1), max_bitmap(1)));
-    map.og_ = cv::Scalar(map.OCCUPIED);
-    floorplan.copyTo(roi);
+
+    cv::imshow("fp_roi", fp_roi);
+    cv::transpose(fp_roi, fp_roi);
+    cv::flip(fp_roi, fp_roi, 1);
+    fp_roi.copyTo(map_roi);
 
     for (cv::MatIterator_<uint8_t> it = map.gt_.begin();
         it != map.gt_.end();
@@ -143,7 +162,7 @@ int main(int argc, char** argv) {
     // trajectory map
     cv::Mat trajectory;
     cv::cvtColor(map.og_, trajectory, cv::COLOR_GRAY2BGR);
-    const double pi = boost::math::constants::pi<double>();
+    //const double pi = boost::math::constants::pi<double>();
     for (r = 0; r < laser_pose.rows; r++) {
         double* pose = laser_pose.ptr<double>(r);
         double* angles = scan_angles.ptr<double>(r);
@@ -155,15 +174,18 @@ int main(int argc, char** argv) {
             bool reflectance;
             ranges[c] = map.ray_trace(pose[0], pose[1], total_angle, MAX_RANGE, final_pos, reflectance);
         }
+        cv::Vec2d position(pose[0], pose[1]);
         // draw input 
         cv::Mat visin;
         cv::cvtColor(map.og_, visin, cv::COLOR_GRAY2BGR);
-        cv::Vec2d position(pose[0], pose[1]);
         map.draw_lasers(visin, position, robot_angle, angles,
             laser_ranges.ptr<double>(r),
             laser_reflectance.ptr<uint8_t>(r),
             scan_angles.cols,
             CV_RGB(0, 255, 0));
+
+        cv::transpose(visin, visin);
+        cv::flip(visin, visin, 0);
         cv::imshow("c", visin);
         cv::imwrite((in_fmter % r).str(), visin);
 
@@ -178,7 +200,10 @@ int main(int argc, char** argv) {
               pt.x, pt.y, 
               pt2.x - pt.x, pt2.y - pt.y,
               cv::Scalar(255, 0, 0));
-          cv::imshow("e", trajectory);
+          cv::Mat trajvis;
+          cv::transpose(trajectory, trajvis);
+          cv::flip(trajvis, trajvis, 0);
+          cv::imshow("e", trajvis);
         }
 
         // draw ground truth
@@ -187,12 +212,19 @@ int main(int argc, char** argv) {
         map.draw_lasers(vis, position, robot_angle, angles, &ranges[0],
             laser_reflectance.ptr<uint8_t>(r),
             scan_angles.cols, CV_RGB(0, 255, 0));
+        cv::transpose(vis, vis);
+        cv::flip(vis, vis, 0);
         cv::imshow("d", vis);
         cv::imwrite((gt_fmter % r).str(), vis);
         cv::waitKey(1);
     }
     std::stringstream ss;
     ss << "groundtruth/" << ++r << ".png";
-    cv::imwrite(ss.str(), map.gt_);
+    cv::Mat vis;
+    cv::transpose(map.gt_, vis);
+    cv::flip(vis, vis, 0);
+    cv::imwrite(ss.str(), vis);
+    cv::transpose(trajectory, trajectory);
+    cv::flip(trajectory, trajectory, 0);
     cv::imwrite("trajectory.png", trajectory);
 }
