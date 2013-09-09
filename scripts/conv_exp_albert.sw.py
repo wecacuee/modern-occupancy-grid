7658bfa9ea4733cc1058f21eed589224dfb12d9b
Vikas Dhiman <vikasdhi@buffalo.edu>
2013-09-09 15:42:51 -0400

    Added directory information to the configuration.

--- scripts/conv_exp_albert.sw.py
+++ scripts/conv_exp_albert.sw.py
@@ -5,6 +5,7 @@ from script_utils import ExecutableConfig
 def executables():
     args = convergence_experiment.exec_config()
     for k, conf in args:
-        conf['args'] = [.05, "--width=25", "--height=25", "--dir=%s" % dir]
+        conf['args'] = [.05, "--width=25", "--height=25",
+                        "--dir=Data/albert.sw/"]
     return [ExecutableConfig(*args) for args in args]
 
