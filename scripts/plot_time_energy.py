9346e8c3d367258c4a1ed82e2c5a4a3b877ec893
vikasdhi <vikasdhi@898a188c-9671-0410-8e00-e3fd810bbb7f>
2013-09-15 23:31:20 +0000

    Changes to the plots.
    git-svn-id: https://svn.cc.gatech.edu/borg/OccupancyGrid/trunk@18946 898a188c-9671-0410-8e00-e3fd810bbb7f

--- scripts/plot_time_energy.py
+++ scripts/plot_time_energy.py
@@ -16,7 +16,10 @@ def _plot_time_energy_two_assumption(exe, times, energy):
 
 if __name__ == '__main__':
     conf = sys.argv[1]
-    fig = plt.figure(figsize=(1.618*3, 3))
+    if "step" in conf:
+        fig = plt.figure(figsize=(1.618*4, 4))
+    else:
+        fig = plt.figure(figsize=(1.618*3, 3))
     fig.subplots_adjust(bottom=0.16)
 
     config = imp.load_source('config', conf)
